5992b911b20b74f030ea9fed6d3a246b06f9a01d
Pivil <pivil@pivils-macbook-pro.home>
2020-11-19 10:32:10 +0100

    create articles (if not already exists) from rss feed

--- api/common/models/article.js
+++ api/common/models/article.js
@@ -1,5 +1,49 @@
 'use strict';
+let Parser = require('rss-parser');
+var Feed = require('./feed')
 
 module.exports = function(Article) {
+    let parser = new Parser();
 
+    Article.createFromRSS = async function() {
+        try {
+            var app = Article.app;
+            await app.models.Feed.find({}, (err, feeds) => {
+                feeds.forEach(async feed => {
+                    let feedData = await parser.parseURL(feed.url);
+                    feedData.items.forEach(item => {
+                        var params = feed.params;
+                        Article.findOrCreate(
+                            {
+                                where: {
+                                    title: item[params.title],
+                                    url: item[params.url],
+                                    date: item[params.date],
+                                    summary: item[params.summary]
+                                }
+                            },
+                            {
+                                title: item[params.title],
+                                url: item[params.url],
+                                date: item[params.date],
+                                summary: item[params.summary]
+                            },
+                            (err, log) => {
+                                if (err) return {status: 400, message: 'Error for article creation from RSS feeds'}
+                            }
+                        );
+                    })
+                })
+            } );
+            return {status: 200, message: 'Articles created from RSS feeds'}
+        } catch (err) {
+            console.log(err);
+        }
+      },
+
+    Article.remoteMethod('createFromRSS', {
+        http: {verb: 'POST'},
+        returns: {type: 'object', root: true}
+
+    });
 };
