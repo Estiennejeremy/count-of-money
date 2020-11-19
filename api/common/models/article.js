8ac36c8a8982a6e774adb2b455c7a20ba992654c
Pivil <pivil@pivils-macbook-pro.home>
2020-11-19 15:17:15 +0100

    create articles from rss feed

--- api/common/models/article.js
+++ api/common/models/article.js
@@ -6,40 +6,39 @@ module.exports = function(Article) {
     let parser = new Parser();
 
     Article.createFromRSS = async function() {
-        try {
-            var app = Article.app;
-            await app.models.Feed.find({}, (err, feeds) => {
-                feeds.forEach(async feed => {
-                    let feedData = await parser.parseURL(feed.url);
-                    feedData.items.forEach(item => {
-                        var params = feed.params;
-                        Article.findOrCreate(
-                            {
-                                where: {
-                                    title: item[params.title],
-                                    url: item[params.url],
-                                    date: item[params.date],
-                                    summary: item[params.summary]
-                                }
-                            },
-                            {
+        var app = Article.app;
+        var feeds = await app.models.Feed.find({});
+        feeds.forEach(async feed => {
+            try {
+                var feedData = await parser.parseURL(feed.url);
+                feedData.items.forEach(item => {
+                    var params = feed.params;
+                    Article.findOrCreate(
+                        {
+                            where: {
                                 title: item[params.title],
                                 url: item[params.url],
                                 date: item[params.date],
                                 summary: item[params.summary]
-                            },
-                            (err, log) => {
-                                if (err) return {status: 400, message: 'Error for article creation from RSS feeds'}
                             }
-                        );
-                    })
+                        },
+                        {
+                            title: item[params.title],
+                            url: item[params.url],
+                            date: item[params.date],
+                            summary: item[params.summary]
+                        },
+                        (err, log) => {
+                            if (err) res = {status: 400, message: 'Error for article creation from RSS feeds'}
+                        }
+                    );
                 })
-            } );
-            return {status: 200, message: 'Articles created from RSS feeds'}
-        } catch (err) {
-            console.log(err);
-        }
-      },
+            } catch (err) {
+                console.error(err);
+            }
+        });
+        return {status: 200, message: 'Done'}
+    },
 
     Article.remoteMethod('createFromRSS', {
         http: {verb: 'POST'},
