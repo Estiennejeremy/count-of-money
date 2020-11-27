97099e689f240fa893cab9035fea54436a5c1bdf
noemie <noemie@noemie.home>
2020-11-27 17:16:58 +0100

    add cryptos table

--- front/src/pages/Cryptos.vue
+++ front/src/pages/Cryptos.vue
@@ -1,13 +1,33 @@
 <template>
   <div class="content">
     <div class="md-layout">
-      coucou cryptos
+      <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-100">
+        <md-card>
+          <md-card-header data-background-color="blue">
+            <h4 class="title">Mes cryptos Money</h4>
+          </md-card-header>
+          <md-card-content>
+            <simple-table table-header-color="green"></simple-table>
+          </md-card-content>
+        </md-card>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+
+import {
+  SimpleTable
+} from '@/components'
+
+
+export default{
+  components: {
+    SimpleTable
+  }
+}
+
 </script>
 
 <style lang="scss" scoped></style>
